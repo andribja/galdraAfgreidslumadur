76e158d40af312b7653ffe9e985f9a8fecebd26a
andribja <andribja@gmail.com>
2015-11-22 17:36:29 +0000

    leidrettari draft

---
+++ leidrettari.py
@@ -0,0 +1,28 @@
+import collections, re, csv
+
+stafrof = re.compile('[a-z\sáðéíótúýþæö]+', re.UNICODE)
+
+with open('althingi_errors/' + name + '.csv') as csvinput:
+    reader = csv.DictReader(csvinput)
+    for row in reader:
+        currword = row['CorrectWord']
+
+def words(text): return stafrof.findall(text)
+
+NWORDS = train(words(file('big.txt').read()))
+
+def train(features):
+    model = collections.defaultdict(lambda: 1)
+    for f in features:
+        model[f] += 1
+    return model
+
+def edits1(word):
+   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+   deletes    = [a + b[1:] for a, b in splits if b]
+   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+   inserts    = [a + c + b     for a, b in splits for c in alphabet]
+   return set(deletes + transposes + replaces + inserts)
+
+
